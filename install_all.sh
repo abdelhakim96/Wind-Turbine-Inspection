8729f5d780c92baba0a09ea79cb5d8faf370ee1b
zartris <jonas.le.fevre@gmail.com>
2021-09-22 12:16:03 +0200

    update

--- install_all.sh
+++ install_all.sh
@@ -1,6 +1,6 @@
 # Fetching the catkin submodule
-@git submodule update --quiet --init --recursive --jobs 4 || true
-@git submodule sync --recursive
-@git submodule update --init --recursive --jobs 4
+git submodule update --quiet --init --recursive --jobs 4 || true
+git submodule sync --recursive
+git submodule update --init --recursive --jobs 4
 
 
